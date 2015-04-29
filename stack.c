b7c78c842909a3cdb8c38412376a49a9e91ca009
Ryan Lafferty <rlafferty1@hotmail.com>
2015-04-28 23:48:20 -0400

    started stack data structure
    building both static and dynamic structures

--- stack.c
+++ stack.c
@@ -6,5 +6,114 @@ Ryan Lafferty
 
 int main(int argc, char *argv[]) 
 {
+	stackNode * test;
 	
+	test = NULL;
+	
+	test = createStaticStack(10);
+	if(test+9 != NULL)
+	{
+		printf("Success! Static Stack was created.\n");
+	}
+	
+	test = destroyStaticStack(test, 10);
+	if(test == NULL)
+	{
+		printf("Success! Static Stack destroyed.\n");
+	}
+	
+	test = createDynamicStack();
+	if(test != NULL)
+	{
+		printf("Success! Dynamic Stack was created.\n");
+	}
+}
+
+stackNode * createStaticStack(int size)
+{
+	stackNode * stack;
+	int i;
+	
+	stack = NULL;
+	i = 0;
+	
+	if(size <= 0)
+	{
+		printf("Error: Size given is too small, must be greater than 0.\n");
+		return NULL;
+	}
+	
+	stackNode mem[size];
+	for(i = 0; i < size; i++)
+	{
+		mem[i].data = 0;
+	}
+	stack = mem;	
+	
+	return stack;
+}
+
+stackNode * destroyStaticStack(stackNode * stack, int size)
+{
+	int i;
+	
+	i = 0;
+	
+	if(stack == NULL)
+	{
+		printf("Error: Cannot destroy an empty stack.\n");
+		return NULL;
+	}
+	
+	for(i = 0; i < size; i++)
+	{
+		if(stack[i].data != 0)
+		{
+			stack[i].data = 0;
+		}
+	}
+	
+	return NULL;
+}
+
+stackNode * createDynamicStack()
+{
+	stackNode * stack;
+	
+	stack = NULL;
+	
+	stack = malloc(sizeof(stackNode) * 1);
+	if(stack == NULL)
+	{
+		printf("Error: Out of memory, stack was not created.\n");
+		return NULL;
+	}
+	
+	stack->data = 0;
+	stack->next = NULL;
+	
+	return stack;
+}
+
+stackNode * destroyDynamicStack(stackNode * stack)
+{
+	stackNode * temp;
+	
+	temp = NULL;
+	
+	if(stack == NULL)
+	{
+		printf("Error: Cannot destroy an empty stack.\n");
+		return NULL;
+	}
+	
+	while(stack->next == NULL)
+	{
+		temp = stack;
+		stack = stack->next;
+		free(temp);	
+	}
+	free(stack);
+	
+	return NULL;
 }
--- stack.h
+++ stack.h
@@ -4,3 +4,28 @@ Ryan Lafferty
 
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct stackNode
+{
+	/*data*/
+	char data;
+	int empty;
+	struct stackNode * next;
+}stackNode;
+
+
+stackNode * createStaticStack(int size);
+stackNode * destroyStaticStack(stackNode * stack, int size);
+
+stackNode * createDynamicStack();
+stackNode * destroyDynamicStack(stackNode * stack);
+
+/*
+listNode * createList();
+listNode * destroyList(listNode * list);
+int addNode(listNode * list, char data);
+listNode * removeNode(listNode * list, char data);
+int destroyNode(listNode * node);
+int findNode(listNode * list, char data);
+listNode * getNode(listNode * list, char data);
+*/
