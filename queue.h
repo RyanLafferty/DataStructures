db46e9266362359adeead283d1ce4c308e97dddf
Ryan Lafferty <rlafferty1@hotmail.com>
2015-05-09 21:35:31 -0400

    init static queue

--- queue.h
+++ queue.h
@@ -18,3 +18,8 @@ queueNode * destroyDynamicQueue(queueNode * queue);
 int dynamicEnqueue(queueNode * queue, queueNode * node);
 int dynamicEnqueueData(queueNode * queue, char data);
 queueNode * dynamicDequeue(queueNode * queue);
+
+queueNode * createStaticStack(int size);
+queueNode * destroyStaticStack(queueNode * stack, int size);
+int staticEnqueue(queueNode * stack, int size, char data);
+queueNode * staticDequeue(stackNode * stack, int size, char data);
--- staticQueue.c
+++ staticQueue.c
@@ -8,3 +8,8 @@ int main(int argc, char *argv[])
 {
 	
 }
+
+queueNode * createStaticStack(int size);
+queueNode * destroyStaticStack(queueNode * stack, int size);
+int staticEnqueue(queueNode * stack, int size, char data);
+queueNode * staticDequeue(stackNode * stack, int size, char data);
